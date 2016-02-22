62fcb7924c3ac513d1afbbcef1b821005f9362c8
Brian Dennis <briandennis321@gmail.com>
2016-02-22 15:39:56 -0600

    added babel files

---
+++ src/main.js
@@ -0,0 +1,8 @@
+document.getElementById('newNoteButton').addEventListener('click',function(){
+  var note = document.createElement('div');
+
+});
+
+let alertFunc = (text) => alert(text);
+
+alertFunc("omg, it worked!");
