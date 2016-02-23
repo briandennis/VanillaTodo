75a087e3582d039fa11f3783aa930e12dd885884
Brian Dennis <briandennis321@gmail.com>
2016-02-23 00:21:56 -0600

    fixed function

--- src/main.js
+++ src/main.js
@@ -2,12 +2,10 @@ let createItem = (text, priority) => {
   let item = document.createElement('div');
   let content = document.createElement('h2');
 
-  content.innerHTML(text);
-  item.appendChild(contents);
+  content.innerHTML = text;
+  item.appendChild(content);
   item.class = 'todoItem';
   document.getElementById('todoContainer').appendChild(item);
-  console.log(content);
-}
+};
 
-
-window.onLoad(createItem('It worked!', 0));
+createItem('Hello, world!');
